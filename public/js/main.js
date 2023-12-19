78f28e011b1d6ccaa6aa34406160d4160f7f2b8d
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2023-12-19 02:22:34 -0300

    Retira informação inutil do codigo

--- public/js/main.js
+++ public/js/main.js
@@ -8,26 +8,6 @@ const c3 = document.querySelector('#c3');
 const caixadado = document.querySelector('#dado');
 
 
-const Jl1c1 = document.querySelector('#Jl1c1');
-const Jl1c2 = document.querySelector('#Jl1c2');
-const Jl1c3 = document.querySelector('#Jl1c3');
-const Jl2c1 = document.querySelector('#Jl2c1');
-const Jl2c2 = document.querySelector('#Jl2c2');
-const Jl2c3 = document.querySelector('#Jl2c3');
-const Jl3c1 = document.querySelector('#Jl3c1');
-const Jl3c2 = document.querySelector('#Jl3c2');
-const Jl3c3 = document.querySelector('#Jl3c3');
-
-const Il1c1 = document.querySelector('#Il1c1');
-const Il1c2 = document.querySelector('#Il1c2');
-const Il1c3 = document.querySelector('#Il1c3');
-const Il2c1 = document.querySelector('#Il2c1');
-const Il2c2 = document.querySelector('#Il2c2');
-const Il2c3 = document.querySelector('#Il2c3');
-const Il3c1 = document.querySelector('#Il3c1');
-const Il3c2 = document.querySelector('#Il3c2');
-const Il3c3 = document.querySelector('#Il3c3');
-
 rodar.addEventListener('click', turnojogador);
 
 function turnojogador() 
